ea143776b98c1afbd06dfc4697aa57382a69fa0b
MGziin <brendonph.trabalho@gmail.com>
2025-10-20 11:04:15 -0300

    Update textos.js

--- textos.js
+++ textos.js
@@ -1,3 +1,4 @@
+/* textos.js — lista de textos (dados) */
 const textos = [
   {
     titulo: "📱 Por que é tão difícil ficar sem celular?",
@@ -20,7 +21,7 @@ Tanto financeiramente falando quanto… sei lá, qualquer outra “coisa falando
 Porque se fosse só por não ter o que fazer, olha…
 minha pia tá cheia de vasilha,
 meu guarda-roupa tá uma bagunça,
-minhas playlists do Spotify tão prontas pra serem atualizadas…
+minhas playlists do Spotify tão prontas para serem atualizadas…
 
 E nossa!
 Parando pra pensar…
@@ -50,8 +51,18 @@ E aí você fica olhando pro nada…
 só olhando…
 e pensando que, no fim…
 talvez o tempo seja o que mais dá falta.`
+  },
+
+  {
+    titulo: "A Semente",
+    categoria: "Fábula",
+    conteudo: `A Semente
+
+Essa é a história de um fazendeiro que tinha um sonho: conseguir uma semente especial. Ele sonhava com ela todos os dias, mas a semente era cara demais para seu bolso.
+Um dia, decidido a realizá-lo, vende todas as sementes que possuía, na esperança de comprar a semente dos sonhos. Mesmo assim, ainda não tinha dinheiro suficiente. Então, vende suas ferramentas de plantio — enxada, pá, regador, adubo — mas ainda assim falta um pouco para alcançar o valor necessário.
+Motivado, ele vende suas terras, os campos onde cultivava suas plantações. Agora, finalmente, consegue o dinheiro e vai à loja todo feliz para comprar a semente. Com ela em mãos, volta para casa, mas percebe algo: abriu mão de tudo para conseguir aquele sonho.
+Perdeu seu maior hobby, a jardinagem, que o fazia feliz todos os dias, e sua própria alimentação, já que dependia do que plantava. Todas as terras, fruto de anos de trabalho, agora estavam longe de seu alcance.
+O valor da semente era imenso para ele — sentimental e monetário —, mas, sem meios para mantê-la, ela acabou apodrecendo com o tempo, tornando-se inútil. O fazendeiro, agora sem plantação, sem sementes e sem dinheiro, se vê perdido, tendo que reconstruir do zero tudo o que havia construído em anos.
+Hum… familiar essa história.`
   }
-
 ];
-
-
